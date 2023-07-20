f384e2608b82380b99545f964e8823bf695a41b5
igor <oreligr2@gmail.com>
2023-07-20 22:28:10 +0200

    commit

---
+++ apps/my-app-1/app/pages/_document.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Document() {
+  return (
+    <html>
+    <head>
+      <title>My Next.js App</title>
+    </head>
+    <body>
+    <main>
+      <h1>Hello, world!</h1>
+    </main>
+    </body>
+    </html>
+  );
+}
